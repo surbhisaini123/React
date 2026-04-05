046ade843d694ee729130b6dfbf3f0f8d9220362
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-04-05 21:14:18 +0530

    react

---
+++ React_Project/src/App.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import './App.css'
+import itle  from "./Title";
+import Title from './Title';
+
+
+const App = () => {
+  return (
+    <div>
+      <Title/>
+
+    </div>
+  
+  )
+}
+
+export default App
+
+
+
---
+++ React_Project/src/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <h1>this is first component</h1>
+  )
+}
+
+export default Title
