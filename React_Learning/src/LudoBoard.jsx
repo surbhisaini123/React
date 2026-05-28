ae2496cf4e1366a8a1e3640d7551349e25a50671
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-05-29 00:10:31 +0530

    react

--- React_Learning/src/LudoBoard.jsx
+++ React_Learning/src/LudoBoard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useState } from "react";
 const LudoBoard = () => {
-    let [move,setMove]=useState({blue:0,green:0,yellow:0,red:0})
+  let [move,setMove]=useState({blue:0,green:0,yellow:0,red:0})  // OBJECT
+  let updateBlue=()=>{
+    // move.blue+=1   OLD WAY TO WRITTEN
+    console.log(`update blue move=${move.blue}`);
+    // setMove({...move, blue:move.blue+=1}) // SPRED AND NEW WAY TO WRITE
+    setMove((prevMove)=>{              // CALL BACK FUNCTION OPTIMIZED WAY TO WRITE 
+          return {...prevMove, blue:prevMove.blue+1}    
+    })
+    
+  }
   return (
     <div>
       <p>Game Begins!!</p>
       <div className='board'>
         <p>Blue move={move.blue}</p>
-        <button style={{backgroundColor:"blue", color:"white"}}>+1</button>
+        <button style={{backgroundColor:"blue", color:"white"}} onClick={updateBlue}>+1</button>
         <p>Green move={move.green}</p>
         <button style={{backgroundColor:"green"}}>+1</button>
         <p>Yellow move={move.yellow}</p>
