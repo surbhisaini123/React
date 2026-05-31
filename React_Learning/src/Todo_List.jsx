f4c7b5eda89c0b462e576f25cbd3529c767c5c36
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-05-31 23:23:58 +0530

    TODO APP

--- React_Learning/src/Todo_List.jsx
+++ React_Learning/src/Todo_List.jsx
@@ -2,18 +2,31 @@ import { useState } from "react";
 import React from 'react'
 
 const Todo_List = () => {
+    let [todo,setTodo]=useState(["sammple task"])
     let [newTodo,setNewTodo]=useState("")
     let addNewTask=()=>{
-        console.log("we have to add new task in todo");
-        
+       setTodo([...todo,newTodo])
+         setNewTodo("")
+    }
+    let updateTodoValue=(event)=>{
+        // console.log(event.target.value);
+       setNewTodo (event.target.value)
+      
+
     }
   return (
     <div>
-            <input placeholder='text' value={newTodo}></input>
+            <input placeholder='text'  value={newTodo} onChange={updateTodoValue}></input>
             <button onClick={addNewTask}>ADD</button>
             <button>DEL</button>
             <h4>TODO APP</h4>
-            <ul></ul>
+            <ul >
+                {
+                    todo.map((todo)=>(
+                        <li>{todo}</li>
+                    ))
+                }
+            </ul>
        
     </div>
   )
