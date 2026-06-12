9c8839b85fbea73ffc58e2749d4048224c15f44b
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-06-12 15:00:18 +0530

    form

--- React_Learning/src/Form.jsx
+++ React_Learning/src/Form.jsx
@@ -13,26 +13,45 @@ const Form = () => {
 
     let [formData,setFormData]=useState({
       fullName:"",
-      userName:""
+      userName:"",
+      password:""
     })
     let handleInputChange=(event)=>{
       let feildName=event.target.name  //CHANGE FEILD
       let newVal=event.target.value
       // console.log(feildName);
-      console.log(newVal);
-      
-      
+      // console.log(newVal);
+      setFormData((currData)=>{
+        // currData[feildName]=newVal  //OLD WAY TO WRITE
+        // return{...currData}
+        return {...currData,[feildName]:newVal}   // NEW WAY TO WRITE
+      })
+    }
+
+    let handleSubmit=(event)=>{
+      event.preventDefault()
+      console.log(formData)
+      setFormData({
+        fullName:"",
+        userName:"",
+        password:"",
+      })
     }
   return (
+
     <div>
-      <form >
+      <form onSubmit={handleSubmit} >
         <label htmlFor='fullName'>full Name</label>
-        <input placeholder="Enter your name" type="text" value={formData.fullName} onChange={handleInputChange} id='userName' name="fullName"/>
-        <button>submit</button>
+        <input placeholder="Enter your name" type="text" value={formData.fullName} onChange={handleInputChange} id='fullName' name="fullName" />
+  
 
       <br></br>
          <label htmlFor='userName'>User Name</label>
-        <input placeholder="Enter your username" type="text" value={formData.userName} onChange={handleInputChange} id='userName' name="userName"/>
+        <input placeholder="Enter your username" type="text" value={formData.userName} onChange={handleInputChange} id='userName' name="userName"  autoComplete='new-password'/>
+        
+         <br></br>
+         <label htmlFor='password'>Password</label>
+        <input placeholder="Enter your password" type="password" value={formData.password} onChange={handleInputChange} id='password' name="password"  autoComplete='new-password' />
         <button>submit</button>
       </form>
       
