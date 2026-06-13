8c931a560e0b8d7f462ecc5cfc68c0b3e5aaaa57
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-06-14 00:39:04 +0530

    react

---
+++ Mini-Project/src/MaterialUI.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
+const MaterialUI = () => {
+  return (
+    <div>
+      <button variant="outlined" disabled >Success</button>
+    </div>
+  )
+}
+
+export default MaterialUI
